696e33893555c89761d301f8fcae8bf1c6cff65b
softquakers <81610881+softquakers@users.noreply.github.com>
2022-08-17 13:39:34 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.post('/SIA', function (req, res) {
+
+    console.log('recieved a request')
+    return res.send({
+        success: true
+    });
+});
+const PORT = 3001
+app.listen(PORT, function(){
+    console.log(`Server listening on port ${PORT}`);
+});
